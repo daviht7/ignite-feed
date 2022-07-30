1aa4c11526a5f78c40804b40a73edac1e42e0128
Davi Holanda <daviht7@gmail.com>
2022-07-30 15:55:02 -0300

    componente sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
 import { Header } from "./components/Header";
+
+import styles from "./App.module.css";
+
+import { Sidebar } from "./components/Sidebar";
 import "./global.css";
+import { Post } from "./Post";
 
 function App() {
-  return <Header />;
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post />
+          <Post />
+        </main>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,21 @@
+import styles from "./Sidebar.module.css";
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1511376777868-611b54f68947?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+      />
+
+      <div className={styles.profile}>
+        <strong>Davi Holanda</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">Editar seu Perfil</a>
+      </footer>
+    </aside>
+  );
+}
